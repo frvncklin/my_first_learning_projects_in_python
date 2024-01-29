5e5c037c0327fc71d1f1c287896b308048c043f2
Victor Francklin <118057237+frvncklin@users.noreply.github.com>
2024-01-29 20:22:30 -0300

    Update jogo da palavra secreta.py

--- jogo da palavra secreta.py
+++ jogo da palavra secreta.py
@@ -10,29 +10,33 @@ while True:
 
     entrada_user = input(">>> ").lower()
 
-    if entrada_user != palavra_secreta:
+    for letra_user in entrada_user:         # Para cada uma das letras do usuário.
 
-        for letra_user in entrada_user:         # Para cada uma das letras do usuário.
+        if letra_user in palavra_secreta and letra_user not in letras_acertadas:       # Verificar se elas estão na palavra secreta.
+            letras_acertadas += letra_user
 
-            if letra_user in palavra_secreta and letra_user not in letras_acertadas:       # Verificar se elas estão na palavra secreta.
-                letras_acertadas += letra_user
+    palavra_formatada = ""
+    tentativas += 1
 
-        palavra_formatada = ""
+    for letra_secreta in palavra_secreta:
 
-        for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formatada += letra_secreta
+        else: 
+            palavra_formatada += "*"
 
-            if letra_secreta in letras_acertadas:
-                palavra_formatada += letra_secreta
-            else: 
-                palavra_formatada += "*"
-
-        if palavra_formatada == palavra_secreta:
+    if palavra_formatada == palavra_secreta:
+        print(f"\n---> Palavra secreta: {palavra_formatada}.\n\nParabéns, você ganhou!\nVocê fez um total de {tentativas} tentativas.")
+        escolha = input("\nDeseja jogar de novo? Digite 1 - sim ou 2 - não.\n>>> ")
+        if escolha == '1':
+            print("................Retornando................\n\n")
+            print("Digite uma tentativa:")
+            letras_acertadas = ""
+            tentativas = 0
+        else:
+            print("\nMuito obrigado e até logo!\nautor: Victor Francklin\n\n.................................")
             break
 
-        tentativas += 1
-        print(f"Palavra secreta: {palavra_formatada}")
-    
     else:
-        break
-
-print(f"Parabéns, você ganhou! A apalvra secreta era {palavra_secreta}!\nVocê fez um total de {tentativas} tentativas.")
+        print(f"Palavra secreta: {palavra_formatada}.")
+        continue
