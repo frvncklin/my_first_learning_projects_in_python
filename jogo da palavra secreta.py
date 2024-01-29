5a00ae0fa94d42cbe7a5717a9f7254ba5ae94ce7
Victor Francklin <118057237+frvncklin@users.noreply.github.com>
2024-01-29 19:56:02 -0300

    Update jogo da palavra secreta.py

--- jogo da palavra secreta.py
+++ jogo da palavra secreta.py
@@ -1,5 +1,5 @@
 palavra_secreta = "coragem"
-palavra_formatada = "*******"
+letras_acertadas = ""
 tentativas = 0
 
 print("------------Jogo da Palavra Secreta------------\n")
@@ -8,19 +8,31 @@ print("Digite uma tentativa:")
 
 while True: 
 
-    entrada_user = input(">>> ")
-    tentativas += 1
+    entrada_user = input(">>> ").lower()
 
     if entrada_user != palavra_secreta:
 
-        for letra_user in entrada_user:
-            if letra_user in palavra_secreta:
-                palavra_formatada = palavra_formatada.replace(palavra_formatada[palavra_secreta.index(letra_user)], letra_user, 1)
-        print(f"Palavra Secreta: {palavra_formatada}")
+        for letra_user in entrada_user:         # Para cada uma das letras do usuário.
+
+            if letra_user in palavra_secreta and letra_user not in letras_acertadas:       # Verificar se elas estão na palavra secreta.
+                letras_acertadas += letra_user
+
+        palavra_formatada = ""
+
+        for letra_secreta in palavra_secreta:
+
+            if letra_secreta in letras_acertadas:
+                palavra_formatada += letra_secreta
+            else: 
+                palavra_formatada += "*"
+
         if palavra_formatada == palavra_secreta:
             break
 
+        tentativas += 1
+        print(f"Palavra secreta: {palavra_formatada}")
+    
     else:
         break
 
-print(f"Parabéns, você ganhou! Você fez um total de {tentativas} tentativas.")
+print(f"Parabéns, você ganhou! A apalvra secreta era {palavra_secreta}!\nVocê fez um total de {tentativas} tentativas.")
