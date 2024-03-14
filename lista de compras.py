dde54a76cd05f9d10b9461b08075634ea2128daa
Victor Francklin <118057237+frvncklin@users.noreply.github.com>
2024-03-13 21:18:15 -0300

    Update lista de compras.py

--- lista de compras.py
+++ lista de compras.py
@@ -5,6 +5,7 @@ count = 0
 item = ''
 verificador = ''       
 verificador_alt = ''
+remover_unico = ''
 contador_erros = 0
 
 import os
@@ -20,19 +21,30 @@ while True:
     print("------------------------------")
     print("             MENU             ")
     print("------------------------------")
-    opcao_menu = input("\n1 - Vizualizar Lista\n2 - Adicionar item\n3 - Apagar item\n\nSelecione uma opção >>> ")
+    opcao_menu = input("\n1 - Vizualizar Lista\n2 - Adicionar item\n3 - Apagar item\n4 - Alterar nome da Lista\n\nSelecione uma opção >>> ")
 
 # Vizualizar lista.
     
     if opcao_menu == "1":
+    
         print('')
-        print(f'\t{nome_lista.upper()}:')
-        print(f'\t', "-" * (len(nome_lista) - 1))
-        for item in enumerate(lista, start=1):
-            indice, nome = item
-            print(f'{indice} --> {nome}')
-        print('------------')
-        print('')
+
+        if len(lista) == 0:
+
+            print("A lista está vazia!\n")
+        
+        else:
+            os.system('cls')
+            print(f'\t{nome_lista.upper()}:')
+            print(f'\t', "-" * (len(nome_lista) - 1))
+
+            for item in enumerate(lista, start=1):
+
+                indice, nome = item
+                print(f'{indice} --> {nome}')
+
+            print('------------')
+            print('')
 
 # Opção de continuar ou não.
         
@@ -47,6 +59,7 @@ while True:
 # Adicionar item à lista.
         
     elif opcao_menu == "2":
+        os.system('cls')
         adicao = input('\n- Digite o item que deseja adicionar à lista \nSe você separa por vírgulas, adicionaremos mais de um!\n(Ex: Sabonete, biscoito, arroz)\n\n>>> ').lower()
         item = ''
         verificador = ''
@@ -128,12 +141,17 @@ while True:
     elif opcao_menu == "3": 
 
         # Visualizando a lista para escolher os itens que quer remover.
+
         print('')
+        os.system('cls')
         print(f'\t{nome_lista.upper()}:')
         print(f'\t', "-" * (len(nome_lista) - 1))
+
         for item in enumerate(lista, start=1):
+
             indice, nome = item
             print(f'{indice} --> {nome}')
+
         print('------------')
         print('')
 
@@ -169,11 +187,12 @@ while True:
 
                     if item_remover.isdigit() == True:
                         
-                        try:
-                            item_remover_int = int(f"{item_remover}") - 1       # Estou fazendo isso pois, no enumerate, os indicies estão aparecendo para o usuário somados de 1, então farei isso para não ocorrer erros nos indices.
-                            removiveis.append(lista[item_remover_int])
+                        item_remover_int = int(f"{item_remover}") - 1       # Estou fazendo isso pois, no enumerate, os indicies estão aparecendo para o usuário somados de 1, então farei isso para não ocorrer erros nos indices.
+
+                        if item_remover_int + 1 <= len(lista):      # Verificando se o índice está na lista.
+                                removiveis.append(lista[item_remover_int])
                         
-                        except:
+                        elif item_remover_int + 1 > len(lista):     # Caso o indice não esteja na lista.
                             contador_erros += 1
                             lista_erros.append(f"índice inexistente digitado: [{item_remover}]")
 
@@ -208,13 +227,15 @@ while True:
 
             if verificador.isdigit() == True:
 
-                try:
-                    verificador_int = int(f"{verificador}") - 1
+                
+                verificador_int = int(f"{verificador}") - 1     # índice real.
+
+                if verificador_int + 1 <= len(lista):
                     removiveis.append(lista[verificador_int])
 
-                except:
+                elif verificador_int + 1 > len(lista):
                     contador_erros += 1
-                    lista_erros.append(f"índice inexistente digitado: [{verificador}]")
+                    lista_erros.append(f"índice inexistente digitado: [{verificador}]")     #índice visto pelo usuário.
 
                 verificador_int = 0
                 verificador = ''
@@ -238,8 +259,62 @@ while True:
             for item_removiveis in removiveis:      # Eliminando os itens identificados da lista.
                 if item_removiveis in lista:
                     lista.remove(item_removiveis)
+            
+            if len(removiveis) > 0:
+                print("\nItens removidos com sucesso!")
+                removiveis = []
 
-        print("\nItens removidos com sucesso!")
+        else:   # Caso seja apenas um item.
+
+            if remover.startswith(" "):
+                for letra_remover_unico in remover:
+
+                     count += 1
+
+                     if count > 1:
+                        remover_unico += letra_remover_unico
+
+                remover = remover_unico
+                remover_unico = ''
+                count = 0
+
+            if remover.isdigit() == True:
+
+                remover_int = int(f"{remover}") - 1
+
+                if remover_int + 1 <= len(lista):
+                    removiveis.append(lista[remover_int])
+                
+                else:
+                    contador_erros += 1
+                    lista_erros.append(f"índice inexistente digitado: [{remover}]")
+
+                remover_unico_int = 0
+                remover = ''
+            
+            else:        # Quando é digitado um único item.
+
+                if remover in lista:
+                    removiveis.append(remover)
+                    remover = ''
+                
+                elif remover not in lista:
+                    contador_erros += 1
+                    lista_erros.append(remover)
+
+            if contador_erros > 0:
+                print(f"\nOs seguintes items não estavam na lista: {lista_erros}. Totalizando {contador_erros} erros.")
+                contador_erros = 0
+                lista_erros = []
+            
+            for item_removiveis in removiveis:      # Eliminando os itens identificados da lista.
+                if item_removiveis in lista:
+                    lista.remove(item_removiveis)
+            
+            if len(removiveis) > 0:
+                print("\nItens removidos com sucesso!")
+                removiveis = []
+          
         print("")
 
         escolha_continuar = input('Continuar - 1 ou sair - 2. \n>>> ')
@@ -249,8 +324,14 @@ while True:
             continue
         else:
             break
-            
+
+    elif opcao_menu == '4':
+        
+        print('')
+        nome_lista = input("Digite o novo nome da lista:\n>>> ")
+        print('')
+        os.system("cls")
+        print("Nome alterado!\n")
 # Caso o usuário digite uma opção inválida:
-    
     else:
         print("\nPor favor digite uma opção válida!\n")
