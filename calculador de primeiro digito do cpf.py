655869345c5db69b7d8f86d37382d7d117120b5b
Victor Francklin <118057237+frvncklin@users.noreply.github.com>
2024-03-26 00:04:04 -0300

    Ferramenta de geração de primeiro dígito do CPF.
    Essa ferramenta simples calcula o primeiro dígito espacial do seu CPF após a vírgula, a partir de uma sequência de números da sua escolha.

---
+++ calculador de primeiro digito do cpf.py
@@ -0,0 +1,72 @@
+import os
+
+lista_numeros_cpf = []
+lista_numeros_cpf_calculo = []
+validador = 10
+determinante_primeiro_digito = 0
+cpf_corrigido = ""
+
+while True:
+
+    cpf = input("Digite os 9 primeiros dígitos do seu CPF (somente números):\n>>> ")
+
+    if len(cpf) != 9:
+
+        os.system("cls")       
+        print("Você precisa digitar todos os 9 dígitos do CPF!\nVamos tentar novamente.\n\n.................\n\n")
+        continue
+
+    elif cpf.isdigit() == False:
+
+        os.system("cls")       
+        print("Há uma letra no CPF, digite somente números!\nVamos tentar novamente.\n\n.................\n\n")
+        continue
+
+    else:
+
+        lista_numeros_cpf = []
+
+        for numero in cpf:
+            lista_numeros_cpf.append(numero)
+        
+        for indice, digito in enumerate(lista_numeros_cpf):
+            digito = int(digito) * validador
+            lista_numeros_cpf_calculo.append(digito)
+            determinante_primeiro_digito += lista_numeros_cpf_calculo[indice]
+            validador -= 1
+
+        digito = 0
+
+        # Realizando operações no determinate do primeiro dígito.
+            
+        determinante_primeiro_digito = (determinante_primeiro_digito * 10) % 11
+
+        primeiro_digito = 0 if determinante_primeiro_digito > 9 else determinante_primeiro_digito
+
+        # Modelando o CPF:
+
+        for indice, digito in enumerate(lista_numeros_cpf):
+
+            cpf_corrigido += str(digito)
+
+            if (indice + 1) % 3 == 0 and (indice + 1) < 9:
+                cpf_corrigido += "."
+            
+            elif indice == 8:
+                cpf_corrigido += "-"
+                cpf_corrigido += str(f"{primeiro_digito}")
+                cpf_corrigido += "X"
+
+        print(f"\n---> O seu CPF será: {cpf_corrigido}")    
+
+    validador = 10          # Reiniciando o valor do validador
+    determinante_primeiro_digito = 0    # Reiniciando o valor do determinante do primeiro dígito.
+
+    escolha = input("\n.........................\nDeseja continuar? 1 - sim ou 2 - não.\n>>> ")
+
+    if escolha.startswith('1'):
+        os.system("cls")
+    else:
+        os.system("cls")
+        print("Encerrando! Muito obrigado!")
+        break
